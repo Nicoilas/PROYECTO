fa01d8786fa9f13b4a68932048dfa24aa2f248a2
Nicolas <nb199144@gmail.com>
2023-10-23 17:38:10 -0500

    laravel

---
+++ Nicolas_proyecto_php/database/migrations/2023_10_23_223105_create_facturas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('facturas', function (Blueprint $table) {
+            $table->id();
+            $table->string('numero_factura',50);
+            $table->string('id_cliente',10);
+            $table->foreignId('id_vendedor')->constrained('vendedores')->onDalate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('facturas');
+    }
+};
